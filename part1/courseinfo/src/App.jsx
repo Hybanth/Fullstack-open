dbfa93d936dfd164a16f5c47a2fb9b24a445a71f
Hybanth <hybanth@gmail.com>
2024-04-16 16:35:16 +0530

    complete step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,15 +10,11 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content
+      part1={part1} exercises1={exercises1} 
+      part2={part2} exercises2={exercises2} 
+      part3={part3} exercises3={exercises3} 
+      />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
@@ -29,5 +25,24 @@ const Header = ({course}) => {
     <h1>{course}</h1>
   )
 }
+
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3} ) => {
+  return (
+    <>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </>
+  );
+}
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+}
+
 
 export default App
