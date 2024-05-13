01077cc4d1afb1a66a004abacf89caaae034a2ed
Hybanth <hybanth@gmail.com>
2024-05-13 11:42:32 +0530

    complted step 9

--- part2/courseInformation/src/App.jsx
+++ part2/courseInformation/src/App.jsx
@@ -14,8 +14,8 @@ const Content = ({parts}) =>
   </div>
 
 const Total = ({exercises}) =>
-  <div>
-     total of {exercises.reduce((total,part) =>total +part.exercises, 0)} exercises 
+  <div><b>
+     total of {exercises.reduce((total,part) =>total +part.exercises, 0)} exercises </b>
   </div>
 
  const Course = ({ course }) =>
@@ -26,29 +26,52 @@ const Total = ({exercises}) =>
     </>
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return courses.map((course) => (<Course key={course.id} course={course} />))
 }
 
 export default App
