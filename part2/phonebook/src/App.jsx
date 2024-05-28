0c439dcae98d1d04f0b7ab60841f697958625da3
Hybanth <hybanth@gmail.com>
2024-05-28 14:00:05 +0530

    installed Axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,10 +5,7 @@ import Persons from './components/Persons';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',number:'1234123412',id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    
   ]) 
   const [newName, setNewName] = useState('')
   const[newNumber, setNewNumber] =useState('')
